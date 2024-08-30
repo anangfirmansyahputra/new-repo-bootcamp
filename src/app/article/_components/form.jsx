e3493ce6cadf827b571e33591592099628a53ab1
anangfirmansyahputra <anangfirmansyahp5@gmail.com>
2024-08-30 10:43:42 +0800

    remove article

--- src/app/article/_components/form.jsx
+++ src/app/article/_components/form.jsx
@@ -98,7 +98,7 @@ export default function Form() {
   return (
     <div>
       {/* <!-- Input Fields --> */}
-      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      {/* <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
           <h3 className="font-medium text-black dark:text-white">
             Article Form
@@ -184,7 +184,7 @@ export default function Form() {
             Submit
           </button>
         </form>
-      </div>
+      </div> */}
     </div>
   );
 }
